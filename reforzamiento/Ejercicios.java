b1b7f49838836e27340b17889ffe1687eeeb4c56
davidmp <mamanipari@gmail.com>
2022-05-20 22:31:58 +0000

    ejemplo 2

--- reforzamiento/Ejercicios.java
+++ reforzamiento/Ejercicios.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
  * Ejercicios
  */
 public class Ejercicios {
-
+    static Scanner leerT=new Scanner(System.in);
     public static void mostrarcursoDia() {
          //Definir Variable
          int diaSem;
          String mensaje;
          String primeraParteM="Ese dia toca el curso de: ";
          //Leer datos 
-         Scanner leerT=new Scanner(System.in);
+         
          System.out.println("Ingrese un dia de la semana:\n1=Dom"+
          "\n2=Lun\n3=Mar\n4=Mie\n5=Jue\n6=Vie\n7=Sab");
          diaSem=leerT.nextInt();
@@ -35,8 +35,30 @@ public class Ejercicios {
          System.out.println(mensaje);
     }
 
+    public static void saludoSegunHora() {
+        //Variables 
+        int hora;
+        String saludo;
+        //datos de entrada
+        System.out.println("Ingrese la hora:");
+        hora=leerT.nextInt();
+        //proceso
+        if (hora>=6 && hora <= 12) {
+            saludo="Buenos dias";
+        } else if(hora>=13 && hora<=20){
+            saludo="Buenos tardes";
+        }else if((hora>=21 && hora <=23) || (hora>=0 && hora<=5)){
+            saludo="Buenos noches";
+        }else{
+            saludo="esa hora no existe"; 
+        }
+        //datos de salida
+        System.out.println(saludo);
+    }
+
     public static void main(String[] args) {
-        mostrarcursoDia();
+        //mostrarcursoDia();
+        saludoSegunHora();
     }
     
 }
