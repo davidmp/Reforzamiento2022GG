d81c07fd6d58fe0a75e8f6bfc22fb3871cc7b3b4
davidmp <mamanipari@gmail.com>
2022-05-27 18:49:02 +0000

    ejercicios reforzamiento

--- refrepetitivas/RefRepetitivas.java
+++ refrepetitivas/RefRepetitivas.java
@@ -30,7 +30,7 @@ public class RefRepetitivas {
     */
     public static void fibonaciN() {
         int nI=0, nS=1, nT;
-        System.out.println("Introduce el N numero:");
+        System.out.println("Introduce  un numero:");
         int n=leerT.nextInt();
         int cont=1;
         if(n>1){
@@ -43,7 +43,75 @@ public class RefRepetitivas {
         }else{nS=n;}
         System.out.println("el factorial de "+n+" es:"+nS);
     }
+    /*
+    Pregunta 3
+    */
+    public static void siNumeroPrimo() {
+        int n,i=1,band=0;
+        System.out.println("Ingrese número: ");
+        n=leerT.nextInt();
+        while (i<=n) {
+            if (n%i == 0) {
+                band++;
+            }
+            i++;
+            if (band>2) { break;}
+        }
 
+        if (band==2) {
+            System.out.println("Es primo");
+        }else{System.out.println("No es primo");}
+    }
+
+    /*
+    Pregunta 6
+    */
+    public static void pintarPiramide() {
+        System.out.println("Ingese Altura de la Letra:"); 
+        int cantF=leerT.nextInt();
+        System.out.println("Ingrese el Caracter:");
+        char caracter=leerT.next().charAt(0);
+        int medio=cantF+1;
+         for (int f = 1; f <= cantF; f++) {
+             for (int c = 1; c <= 2*cantF+1; c++) {
+                 if (f==1 && medio==c) {
+                     System.out.print(caracter);
+                 }else if(c>=medio-f && f>1 && c<=medio+f ){
+                     System.out.print(caracter);
+                 }else if(f==cantF){
+                     System.out.print(caracter);
+                 }else{
+                     System.out.print(" ");
+                 }
+             }
+             System.out.print("\n");
+         }
+         System.out.println("");
+    }    
+
+ /*
+    Pregunta 10
+    */
+    public static void pintarU() {
+        System.out.println("Ingese Altura de la Letra:"); 
+        int cantF=leerT.nextInt();
+        System.out.println("Ingrese el Caracter:");
+        char caracter=leerT.next().charAt(0);
+        
+         for (int f = 1; f <= cantF; f++) {
+             for (int c = 1; c <= cantF; c++) {
+                 if ((c==1 || c==cantF)  && f<cantF) {
+                     System.out.print(caracter);
+                 }else if(f==cantF && c>1 && c<=cantF-1 ){
+                     System.out.print(caracter);
+                 }else{
+                     System.out.print(" ");
+                 }
+             }
+             System.out.print("\n");
+         }
+         System.out.println("");
+    }
 
     public static void main(String[] args) {
         System.out.println("Introduce Numero algoritmo:");
@@ -51,6 +119,10 @@ public class RefRepetitivas {
         switch (opc) {
             case 1: serieFibonaci(); break;
             case 2: fibonaciN(); break;
+            case 3: siNumeroPrimo(); break;
+            case 4: pintarPiramide(); break;
+            case 5: pintarU(); break;
+
         
             default:break;
         }
